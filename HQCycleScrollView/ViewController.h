324d7ba96b49a85c33978f7a0c5ef24599d53e3e
Vesincc <1464467970@qq.com>
2017-08-23 10:18:16 +0800

    init

---
+++ HQCycleScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQCycleScrollView
+//
+//  Created by HanQi on 2017/8/23.
+//  Copyright © 2017年 HanQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
